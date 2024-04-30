36611be20e7695c4d7ef7fd6f9886d1153962c20
Aditi Khare <89248363+Aditikhare1101@users.noreply.github.com>
2024-04-30 23:58:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,419 +1,210 @@
-import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
-import React from "react";
-import { LinearGradient } from "expo-linear-gradient";
-import { Feather, Entypo, Ionicons, Octicons } from "@expo/vector-icons";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { useRouter } from "expo-router";
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const moment = require("moment");
 
-const index = () => {
-    const router = useRouter();
-  return (
-    <ScrollView>
-      <LinearGradient colors={["#7F7FD5", "#E9E4F0"]} style={{ flex: 1 }}>
-        <View style={{ padding: 12 }}>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Feather name="bar-chart" size={24} color="black" />
-            <Text style={{ fontSize: 16, fontWeight: "600" }}>
-              Employee Managament System
-            </Text>
-            <Entypo name="lock" size={24} color="black" />
-          </View>
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
 
-          <View
-            style={{
-              marginTop: 20,
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 20,
-            }}
-          >
-            <Pressable
-           onPress={() => router.push("/(home)/employees")}
-              style={{
-                backgroundColor: "#D3CCE3",
-                padding: 12,
-                borderRadius: 6,
-                alignItems: "center",
-                justifyContent: "center",
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  width: 50,
-                  height: 50,
-                  borderRadius: 25,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Ionicons name="ios-people-sharp" size={24} color="black" />
-              </View>
-              <Text style={{ marginTop: 7, fontWeight: "600" }}>
-                Employee List
-              </Text>
-            </Pressable>
-            <Pressable
-              onPress={() => router.push("/(home)/markattendance")}
-              style={{
-                backgroundColor: "#D3CCE3",
-                padding: 12,
-                borderRadius: 6,
-                alignItems: "center",
-                justifyContent: "center",
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  width: 50,
-                  height: 50,
-                  borderRadius: 25,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Ionicons name="ios-people-sharp" size={24} color="black" />
-              </View>
-              <Text style={{ marginTop: 7, fontWeight: "600" }}>
-                Mark Attendance
-              </Text>
-            </Pressable>
-          </View>
-          <View
-            style={{
-              marginTop: 20,
-              backgroundColor: "white",
-              paddingHorizontal: 10,
-              paddingVertical: 10,
-              borderRadius: 7,
-            }}
-          >
-            <Pressable
-              onPress={() => router.push("/(home)/attendanceReport")}
-              style={{
-                backgroundColor: "#BE93C5",
-                borderRadius: 6,
-                padding: 10,
-                flexDirection: "row",
-                alignItems: "center",
-                marginVertical: 10,
-              }}
-            >
-              <View
-                style={{
-                  padding: 7,
-                  width: 45,
-                  height: 45,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Ionicons name="newspaper-outline" size={24} color="black" />
-              </View>
-              <Text
-                style={{
-                  marginLeft: 10,
-                  fontSize: 16,
-                  fontWeight: "600",
-                  flex: 1,
-                }}
-              >
-                Attendance Report
-              </Text>
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Entypo name="chevron-right" size={24} color="black" />
-              </View>
-            </Pressable>
-            <Pressable
-                onPress={() => router.push("/(home)/summary")}
-              style={{
-                backgroundColor: "#BE93C5",
-                borderRadius: 6,
-                padding: 10,
-                flexDirection: "row",
-                alignItems: "center",
-                marginVertical: 10,
-              }}
-            >
-              <View
-                style={{
-                  padding: 7,
-                  width: 45,
-                  height: 45,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Octicons name="repo-pull" size={24} color="black" />
-              </View>
-              <Text
-                style={{
-                  marginLeft: 10,
-                  fontSize: 16,
-                  fontWeight: "600",
-                  flex: 1,
-                }}
-              >
-                Summary Report
-              </Text>
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Entypo name="chevron-right" size={24} color="black" />
-              </View>
-            </Pressable>
-            <Pressable
-              style={{
-                backgroundColor: "#BE93C5",
-                borderRadius: 6,
-                padding: 10,
-                flexDirection: "row",
-                alignItems: "center",
-                marginVertical: 10,
-              }}
-            >
-              <View
-                style={{
-                  padding: 7,
-                  width: 45,
-                  height: 45,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Octicons name="report" size={24} color="black" />
-              </View>
-              <Text
-                style={{
-                  marginLeft: 10,
-                  fontSize: 16,
-                  fontWeight: "600",
-                  flex: 1,
-                }}
-              >
-                All Generate Reports
-              </Text>
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Entypo name="chevron-right" size={24} color="black" />
-              </View>
-            </Pressable>
-            <Pressable
-              style={{
-                backgroundColor: "#BE93C5",
-                borderRadius: 6,
-                padding: 10,
-                flexDirection: "row",
-                alignItems: "center",
-                marginVertical: 10,
-              }}
-            >
-              <View
-                style={{
-                  padding: 7,
-                  width: 45,
-                  height: 45,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Ionicons name="people" size={24} color="black" />
-              </View>
-              <Text
-                style={{
-                  marginLeft: 10,
-                  fontSize: 16,
-                  fontWeight: "600",
-                  flex: 1,
-                }}
-              >
-                Overtime Employees
-              </Text>
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Entypo name="chevron-right" size={24} color="black" />
-              </View>
-            </Pressable>
-          </View>
-          <View
-            style={{
-              marginTop: 20,
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 12,
-            }}
-          >
-            <View
-              style={{
-                backgroundColor: "#f79d00",
-                borderRadius: 6,
-                padding: 12,
-                alignItems: "center",
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-                justifyContent: "center",
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <MaterialCommunityIcons
-                  name="guy-fawkes-mask"
-                  size={24}
-                  color="black"
-                />
-              </View>
-              <Text style={{ marginTop: 7 }}>Attendance Criteria</Text>
-            </View>
-            <View
-              style={{
-                backgroundColor: "#ABCABA",
-                borderRadius: 6,
-                padding: 12,
-                alignItems: "center",
-                justifyContent: "center",
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Feather name="bar-chart" size={24} color="black" />
-              </View>
-              <Text style={{ marginTop: 7 }}>Increased Workflow</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              marginTop: 20,
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 12,
-            }}
-          >
-            <View
-              style={{
-                backgroundColor: "#D3CCE3",
-                borderRadius: 6,
-                padding: 12,
-                alignItems: "center",
+mongoose
+  .connect("mongodb+srv://Aditi:aditi@cluster0.b7ofuve.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error);
+  });
 
-                justifyContent: "center",
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <MaterialCommunityIcons
-                  name="guy-fawkes-mask"
-                  size={24}
-                  color="black"
-                />
-              </View>
-              <Text style={{ marginTop: 7 }}>Cost Savings</Text>
-            </View>
-            <View
-              style={{
-                backgroundColor: "#bdc3c7",
-                borderRadius: 6,
-                padding: 12,
-                alignItems: "center",
-                justifyContent: "center",
-                flex: 1,
-              }}
-            >
-              <View
-                style={{
-                  width: 35,
-                  height: 35,
-                  borderRadius: 7,
-                  backgroundColor: "white",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Feather name="bar-chart" size={24} color="black" />
-              </View>
-              <Text style={{ marginTop: 7 }}>Employee Performance</Text>
-            </View>
-          </View>
-        </View>
-      </LinearGradient>
-    </ScrollView>
-  );
-};
+app.listen(port, () => {
+  console.log("Server is running on port 8000");
+});
 
-export default index;
+const Employee = require("./models/employee");
+const Attendance = require("./models/attendance");
 
-const styles = StyleSheet.create({});
+//endpoint to register a employee
+app.post("/addEmployee", async (req, res) => {
+  try {
+    const {
+      employeeName,
+      employeeId,
+      designation,
+      phoneNumber,
+      dateOfBirth,
+      joiningDate,
+      activeEmployee,
+      salary,
+      address,
+    } = req.body;
+
+    //create a new Employee
+    const newEmployee = new Employee({
+      employeeName,
+      employeeId,
+      designation,
+      phoneNumber,
+      dateOfBirth,
+      joiningDate,
+      activeEmployee,
+      salary,
+      address,
+    });
+
+    await newEmployee.save();
+
+    res
+      .status(201)
+      .json({ message: "Employee saved successfully", employee: newEmployee });
+  } catch (error) {
+    console.log("Error creating employee", error);
+    res.status(500).json({ message: "Failed to add an employee" });
+  }
+});
+
+//endpoint to fetch all the employees
+app.get("/employees", async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.status(200).json(employees);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve the employees" });
+  }
+});
+
+app.post("/attendance", async (req, res) => {
+  try {
+    const { employeeId, employeeName, date, status } = req.body;
+
+    const existingAttendance = await Attendance.findOne({ employeeId, date });
+
+    if (existingAttendance) {
+      existingAttendance.status = status;
+      await existingAttendance.save();
+      res.status(200).json(existingAttendance);
+    } else {
+      const newAttendance = new Attendance({
+        employeeId,
+        employeeName,
+        date,
+        status,
+      });
+      await newAttendance.save();
+      res.status(200).json(newAttendance);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error submitting attendance" });
+  }
+});
+
+app.get("/attendance", async (req, res) => {
+  try {
+    const { date } = req.query;
+
+    // Find attendance records for the specified date
+    const attendanceData = await Attendance.find({ date: date });
+
+    res.status(200).json(attendanceData);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching attendance data" });
+  }
+});
+
+app.get("/attendance-report-all-employees", async (req, res) => {
+    try {
+      const { month, year } = req.query;
+  
+      console.log("Query parameters:", month, year);
+      // Calculate the start and end dates for the selected month and year
+      const startDate = moment(`${year}-${month}-01`, "YYYY-MM-DD")
+        .startOf("month")
+        .toDate();
+      const endDate = moment(startDate).endOf("month").toDate();
+  
+      // Aggregate attendance data for all employees and date range
+      const report = await Attendance.aggregate([
+        {
+          $match: {
+            $expr: {
+              $and: [
+                {
+                  $eq: [
+                    { $month: { $dateFromString: { dateString: "$date" } } },
+                    parseInt(req.query.month),
+                  ],
+                },
+                {
+                  $eq: [
+                    { $year: { $dateFromString: { dateString: "$date" } } },
+                    parseInt(req.query.year),
+                  ],
+                },
+              ],
+            },
+          },
+        },
+  
+        {
+          $group: {
+            _id: "$employeeId",
+            present: {
+              $sum: {
+                $cond: { if: { $eq: ["$status", "present"] }, then: 1, else: 0 },
+              },
+            },
+            absent: {
+              $sum: {
+                $cond: { if: { $eq: ["$status", "absent"] }, then: 1, else: 0 },
+              },
+            },
+            halfday: {
+              $sum: {
+                $cond: { if: { $eq: ["$status", "halfday"] }, then: 1, else: 0 },
+              },
+            },
+            holiday: {
+              $sum: {
+                $cond: { if: { $eq: ["$status", "holiday"] }, then: 1, else: 0 },
+              },
+            },
+          },
+        },
+        {
+          $lookup: {
+            from: "employees", // Name of the employee collection
+            localField: "_id",
+            foreignField: "employeeId",
+            as: "employeeDetails",
+          },
+        },
+        {
+          $unwind: "$employeeDetails", // Unwind the employeeDetails array
+        },
+        {
+          $project: {
+            _id: 1,
+            present: 1,
+            absent: 1,
+            halfday: 1,
+            name: "$employeeDetails.employeeName",
+            designation:"$employeeDetails.designation",
+            salary: "$employeeDetails.salary",
+            employeeId: "$employeeDetails.employeeId",
+          },
+        },
+      ]);
+  
+      res.status(200).json({ report });
+    } catch (error) {
+      console.error("Error generating attendance report:", error);
+      res.status(500).json({ message: "Error generating the report" });
+    }
+  });
+  
